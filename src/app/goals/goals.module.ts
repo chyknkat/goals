796e6b686d83e0f30c1e48282b34ced96a5aa953
katrina <chykn75@gmail.com>
2019-07-14 10:18:16 -0400

    Goal details, edit goal, and add goal components

--- src/app/goals/goals.module.ts
+++ src/app/goals/goals.module.ts
@@ -3,15 +3,18 @@ import { CommonModule } from '@angular/common';
 import { GoalsComponent } from './goals/goals.component';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { MatIconModule } from '@angular/material';
+import { AddGoalComponent } from './add-goal/add-goal.component';
+import { EditGoalComponent } from './edit-goal/edit-goal.component';
+import { GoalDetailComponent } from './goal-detail/goal-detail.component';
 
 
 @NgModule({
-  declarations: [GoalsComponent],
+  declarations: [GoalsComponent, AddGoalComponent, EditGoalComponent, GoalDetailComponent],
   imports: [
     CommonModule,
     FlexLayoutModule,
     MatIconModule
   ],
-  exports: [GoalsComponent]
+  exports: [GoalsComponent, AddGoalComponent, EditGoalComponent, GoalDetailComponent]
 })
 export class GoalsModule { }
